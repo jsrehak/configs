9699c3c5e5ea4f5bd939529913b4832775decd7e
Joshua Rehak <jsrehak@gmail.com>
2018-07-06 10:37:06 -0700

    corrected volume change scripts and keybindings

--- .scripts/volume.sh
+++ .scripts/volume.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
- amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off"){print "  "}else if($2 <=50){print " "$2}else{print " "$2}}' | tail -n 1
+# amixer get Master | awk -F'[]%[]' '/%/ {if ($7 == "off"){print "  "}else if($2 <=50){print " "$2}else{print " "$2}}' | tail -n 1
+ amixer -c 1 get Master | awk -F'[]%[]' '/%/ {if ($7 == "off"){print "Muted "}else if($2 <=50){print " "$2}else{print " "$2}}' | tail -n 1
